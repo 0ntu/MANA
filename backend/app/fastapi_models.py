c83607c933ced7f781278923101a132c23a1ed8e
adam-crow <adam.thecrows@gmail.com>
2026-03-07 19:09:15 -0500

    Create fastapi_models.py

---
+++ backend/app/fastapi_models.py
@@ -0,0 +1,2 @@
+#we need models for the inpuyts so that fastapi can auto validate them otherwise its a lotta code
+# useful resource link: https://docs.pydantic.dev/latest/concepts/models/#generic-models
