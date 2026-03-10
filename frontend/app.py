6de8e219410bc680fab203205091c13d8d813215
Alex Escalera <escaleraalex8@gmail.com>
2026-03-09 21:45:28 -0400

    added files and started front page of website

--- frontend/app.py
+++ frontend/app.py
@@ -1,37 +1,6 @@
 import streamlit as st
-from streamlit.logger import get_logger
+st.set_page_config(page_title="UF MANA", page_icon=":robot_face:", layout="wide")
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+st.title("UF MANA")
+st.caption("Energy-aware scheduling for students who can't afford burnout")
+st.sidebar.success("Welcome to UF MANA! Please select a page from the sidebar to get started.")
